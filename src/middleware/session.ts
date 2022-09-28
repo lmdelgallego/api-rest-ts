b9641d390f079647f42fb445e5b5611923c83bf1
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-09-27 21:34:54 -0500

    Improve route for upload file

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -1,17 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { JwtPayload } from "jsonwebtoken";
-import { handleHttp } from "../utils/error.handle";
+import { RequestExt } from "../interface/req-ext";
 import { verifyToken } from "../utils/jwt.handle";
 
-interface RequestExtended extends Request {
-  user?: string | JwtPayload;
-}
 
-const checkJwt =  (req: RequestExtended, res: Response, next: NextFunction) => {
+const checkJwt =  (req: RequestExt, res: Response, next: NextFunction) => {
   try {
     const jwtByUser = req.headers.authorization || '';
     const jwt = jwtByUser.split(' ').pop();
-    const isUser = verifyToken(`${jwt}`);
+    const isUser = verifyToken(`${jwt}`) as { id: string };
     console.log(isUser)
     if (!isUser) {
       res.status(401).send({
--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getFile } from "../controllers/upload";
 import multerMiddleware from "../middleware/file";
+import { checkJwt } from "../middleware/session";
 
 const router = Router();
 
-router.post("/", multerMiddleware.single('myfile'), getFile);
+router.post("/", checkJwt ,multerMiddleware.single('myfile'), getFile);
 
 export { router };
